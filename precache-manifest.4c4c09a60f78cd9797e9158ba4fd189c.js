7685e5ec7af597943d9aee1734b6f4346ce5ead6
Sean Donnellan <sean@seandonn.io>
2020-07-08 15:44:38 -0400

    Updates

---
+++ precache-manifest.4c4c09a60f78cd9797e9158ba4fd189c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d7665107f5347311a280cc0b67aa06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562aa1a283d7299fc9d3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "076d2b0b73a85b82abdf",
+    "url": "/static/js/2.019a7d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.019a7d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562aa1a283d7299fc9d3",
+    "url": "/static/js/main.527cb382.chunk.js"
+  },
+  {
+    "revision": "30242e50035f15ef3401",
+    "url": "/static/js/runtime-main.5a5d5a36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
