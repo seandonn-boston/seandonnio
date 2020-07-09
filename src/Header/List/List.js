fbb9e3f058c160bb4562fb296ce1a19b5b868a6f
Sean Donnellan <sean@seandonn.io>
2020-07-09 16:56:32 -0400

    Added proptypes

--- src/Header/List/List.js
+++ src/Header/List/List.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const List = ({ children: child }) => <ul className="List">{child}</ul>;
 
 export default List;
+
+List.propTypes = {
+  child: PropTypes.instanceOf(Element).isRequired,
+};
--- src/Header/ListItem/ListItem.js
+++ src/Header/ListItem/ListItem.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const ListItem = ({ value }) => <li className="ListItem">{value}</li>;
 
 export default ListItem;
+
+ListItem.propTypes = {
+  value: PropTypes.string.isRequired,
+};
