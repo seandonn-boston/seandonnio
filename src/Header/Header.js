8101113482e40b801bf45a1b82eba568a6bd1208
Sean Donnellan <sean@seandonn.io>
2020-07-13 12:57:26 -0400

    Testing sass

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,9 +2,10 @@ import React from "react";
 import Logo from "./Logo/Logo";
 import Nav from "./Nav/Nav";
 import Button from "../Utilities/Button/Button";
+import styles from "./Header.scss";
 
 const Header = () => (
-  <div className="Header">
+  <div className="Header Header-test">
     <Logo />
     <Nav />
     <Button content="Resume" classes="Button--resume" />
