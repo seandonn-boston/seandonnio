c93f6acc8d98ebfc27ea4c18acd511b99a6549db
Sean Donnellan <sean@seandonn.io>
2020-07-15 17:48:12 -0400

    Fix App.js to reconnect Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Header from "./Header/Header";
 import "./App.scss";
 
 function App() {
-  return <div className="App"><Header/></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
