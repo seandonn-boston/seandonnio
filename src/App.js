8228e495381017ff0ef42919e71f9f1a09e31bec
Sean Donnellan <sean@seandonn.io>
2020-09-14 16:39:46 -0400

    Include redux toolkit, rename folder structure slightly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Header from "./Header/Header";
-import Veil from "./ubiquitous/components/Veil/Veil";
+import Navigation from "./Navigation/Navigation";
+import Veil from "./ubiquitous/ui/Veil/Veil";
 import useWindowSize from "./ubiquitous/hooks/useWindowSize";
 import { app } from "./App.scss";
 
@@ -10,19 +10,19 @@ const App = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   const isMobile = useWindowSize().width < MOBILE_MAX_WIDTH;
   return (
-    <div className={app}>
-      <Header
-        isMobile={isMobile}
-        isMobileNavOpen={isMobileNavOpen}
-        setIsMobileNavOpen={setIsMobileNavOpen}
-      />
-      {isMobile && isMobileNavOpen && (
-        <Veil
+      <div className={app}>
+        <Navigation
+          isMobile={isMobile}
           isMobileNavOpen={isMobileNavOpen}
           setIsMobileNavOpen={setIsMobileNavOpen}
         />
-      )}
-    </div>
+        {isMobile && isMobileNavOpen && (
+          <Veil
+            isMobileNavOpen={isMobileNavOpen}
+            setIsMobileNavOpen={setIsMobileNavOpen}
+          />
+        )}
+      </div>
   );
 };
 
--- src/Header/Logo/Logo.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { logo } from "./Logo.scss";
-
-const Logo = () => <span className={logo} />;
-
-export default Logo;
