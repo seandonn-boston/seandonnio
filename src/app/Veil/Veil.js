4f46be7e9daa80f23c38602d515c461f0330334f
Sean Donnellan <sean@seandonn.io>
2020-10-30 15:44:41 -0400

    First resolution of utilizing thunks for multiple dispatches and access to state

--- src/app/Veil/Veil.js
+++ src/app/Veil/Veil.js
@@ -2,44 +2,17 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import cx from "classnames";
 
-import { veilOpener, selectVeilIsOpen } from "./veilSlice";
-import {
-  navLinksOpener,
-  selectNavLinksIsOpen,
-} from "../Navigation/NavLinks/navLinksSlice";
-import {
-  setBurgerIsActive,
-  selectBurgerIsActive,
-} from "../../global/ui/Burger/burgerSlice";
-import {
-  modalOpener,
-  selectModalIsOpen,
-  selectModalContentFile,
-  setModalContentType,
-  setModalContentFile,
-} from "../Modal/modalSlice";
+import { handleVeilOnClick } from "./veilSlice";
+import { selectNavLinksIsOpen } from "../Navigation/NavLinks/navLinksSlice";
 
 import { veil, veilBehindNavLinks } from "./Veil.scss";
 
 export default function Veil() {
   const isNavLinksOpen = useSelector(selectNavLinksIsOpen);
-  const isBurgerActive = useSelector(selectBurgerIsActive);
-  const isModalOpen = useSelector(selectModalIsOpen);
-  const isVeilOpen = useSelector(selectVeilIsOpen);
-  const modalContentFile = useSelector(selectModalContentFile);
 
   const dispatch = useDispatch();
 
-  const handleOnClick = () => {
-    isVeilOpen && dispatch(veilOpener());
-    isNavLinksOpen && dispatch(navLinksOpener());
-    isBurgerActive && dispatch(setBurgerIsActive());
-    if (isModalOpen) {
-      dispatch(modalOpener());
-      dispatch(setModalContentType(""));
-      modalContentFile && dispatch(setModalContentFile(null));
-    }
-  };
+  const handleOnClick = () => dispatch(handleVeilOnClick());
 
   const veilClasses = cx(veil, {
     [veilBehindNavLinks]: isNavLinksOpen,
--- src/app/Veil/veilSlice.js
+++ src/app/Veil/veilSlice.js
@@ -1,5 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { navLinksOpener } from "../Navigation/NavLinks/navLinksSlice";
+import { setBurgerIsActive } from "../../global/ui/Burger/burgerSlice";
+import {
+  modalOpener,
+  setModalContentFile,
+  setModalContentType,
+} from "../Modal/modalSlice";
+
+export function handleVeilOnClick() {
+  return (dispatch, getState) => {
+    const {
+      veil: { isOpen: veilIsOpen },
+      navLinks: { isOpen: navLinksIsOpen },
+      burger: { isActive: burgerIsActive },
+      modal: {
+        isOpen: modalIsOpen,
+        content: { type: modalContentType, file: modalContentFile },
+      },
+    } = getState();
+    veilIsOpen && dispatch(veilOpener());
+    navLinksIsOpen && dispatch(navLinksOpener());
+    burgerIsActive && dispatch(setBurgerIsActive());
+    if (modalIsOpen) {
+      dispatch(modalOpener());
+      modalContentType && dispatch(setModalContentType(""));
+      modalContentFile && dispatch(setModalContentFile(null));
+    }
+  };
+}
+
 export const veilSlice = createSlice({
   name: "veil",
   initialState: {
