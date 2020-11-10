d9029e80965ff3cda3d43549544ae93ba4cbe0da
Sean Donnellan <sean@seandonn.io>
2020-11-10 12:33:23 -0500

    Attempt tofix BrowserRouter deployed 404 refresh error.

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 import Navigation from "./Navigation/Navigation";
 import Modal from "./Modal/Modal";
 import Veil from "./Veil/Veil";
@@ -9,16 +8,14 @@ import AboutPage from "../features/AboutPage/AboutPage";
 import ContactPage from "../features/ContactPage/ContactPage";
 import PortfolioPage from "../features/PortfolioPage/PortfolioPage";
 import HomePage from "../features/HomePage/HomePage";
-
 import { selectVeilIsOpen } from "./Veil/veilSlice";
 import { selectModalIsOpen } from "./Modal/modalSlice";
-
 import useMatchMediaQueries from "../global/hooks/useMatchMediaQueries";
-
 import { app } from "./App.scss";
 
 export default function App() {
   // TODO: Can useMatchMediaQueries be moved to index.js?
+  // perhaps it can be loaded in the store.js file somehow?
   // IMPORTANT: useMatchMediaQueries can, should, and must only be called here once in the entire codebase
   // useMatchMediaQueries initializes window.matchMedia event listener via useEffect, thus tethering mobile conditionals in JS directly to their CSS media queries and their breakpoints. BONUS: you can export scss breakpoint variables and import them in JS
   useMatchMediaQueries();
@@ -27,7 +24,7 @@ export default function App() {
   const isModalOpen = useSelector(selectModalIsOpen);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Navigation />
       {isModalOpen && <Modal />}
       {isVeilOpen && <Veil />}
