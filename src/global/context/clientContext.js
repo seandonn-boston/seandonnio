a0cc155b0d1f8208484117f57afb0e2622978c02
Sean Donnellan <sean@seandonn.io>
2020-11-29 17:56:33 -0500

    update clientContext to be more efficient.

--- src/global/context/clientContext.js
+++ src/global/context/clientContext.js
@@ -1,43 +1,62 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useCallback } from "react";
 
-import { bpM, bpL, bpMpx, bpLpx } from "../styles/config/_breakpoints.scss";
+import { bpMpx, bpLpx } from "../styles/config/_breakpoints.scss";
 
 export const ClientContext = createContext();
 
 export const ClientContextProvider = ({ children }) => {
-  // isClient flag to ensure user is viewing website on a browser
+  // ensure user is viewing website on a browser
   const isClient = typeof window === "object";
 
-  const [isMobile, setIsMobile] = useState(isClient && window.innerWidth < bpM);
-  const [isTablet, setIsTablet] = useState(
-    isClient && window.innerWidth < bpL && window.innerWidth > bpM
-  );
+  // TODO: move to breakpoints css
+  const queries = [
+    `(min-width: ${bpMpx}) and (max-width: ${bpLpx})`,
+    `(min-width: ${bpLpx})`,
+  ];
 
-  const handleOnChange = () => {
-    setIsMobile(window.innerWidth < bpM);
-    setIsTablet(window.innerWidth < bpL && window.innerWidth > bpM);
-  };
+  // Listener for JS to match the css media queries on resize events and initial page load without the need to debounce duplicate calls
+  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+
+  const getValue = useCallback(() => {
+    const index = mediaQueryLists.findIndex((mql) => mql.matches);
+    switch (index) {
+      case 0:
+        return "isTablet";
+      case 1:
+        return "isDesktop";
+      default:
+        return "isMobile";
+    }
+  }, [mediaQueryLists]);
+
+  const [value, setValue] = useState(getValue);
+
+  const handleEvent = useCallback(() => {
+    setValue(getValue());
+  }, [getValue]);
 
   useEffect(() => {
     if (!isClient) {
       return;
     }
-    // Listener for JS to match the css media queries on resize events and initial page load without the need to debounce duplicate calls
-    const mediaQueryM = window.matchMedia(`(min-width: ${bpMpx})`);
-    const mediaQueryL = window.matchMedia(`(min-width: ${bpLpx})`);
 
-    mediaQueryM.addEventListener("change", handleOnChange);
-    mediaQueryL.addEventListener("change", handleOnChange);
-    window.addEventListener("orientationchange", handleOnChange);
+    mediaQueryLists.forEach((mql) =>
+      mql.addEventListener("change", handleEvent)
+    );
+    // Changing the orientation does not always trigger the matchMedia listeners, so an additional orientation listener is required
+    window.addEventListener("orientationchange", handleEvent);
     return () => {
-      mediaQueryM.removeEventListener("change", handleOnChange);
-      mediaQueryL.removeEventListener("change", handleOnChange);
-      window.removeEventListener("orientationchange", handleOnChange);
+      mediaQueryLists.forEach((mql) =>
+        mql.removeEventListener("change", handleEvent)
+      );
+      window.removeEventListener("orientationchange", handleEvent);
     };
-  });
+  }, [value, handleEvent, isClient, mediaQueryLists]);
 
   return (
-    <ClientContext.Provider value={{ isMobile: isMobile, isTablet: isTablet }}>
+    <ClientContext.Provider
+      value={{ isMobile: value === "isMobile", isTablet: value === "isTablet" }}
+    >
       {children}
     </ClientContext.Provider>
   );
