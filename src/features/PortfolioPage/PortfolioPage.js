bafb1bbbf60d098fd9f7feab15625ea355f5aa52
Sean Donnellan <sean@seandonn.io>
2021-01-19 13:32:07 -0500

    Add comments to clarify functions

--- src/features/PortfolioPage/PortfolioPage.js
+++ src/features/PortfolioPage/PortfolioPage.js
@@ -21,13 +21,17 @@ let tagsMap = new Map(),
   array10k = Array.from({ length: 10000 });
 [...array10k].forEach((_, i) => {
   tagsMap.set(`${i}`, {
+    // selected is determined by enter key or use clicks - if true, hide the item in the dropdown
     isSelected: false,
+    // filtered is determined by the user inputsearch input value - if true, hide this item in the dropdown
     isFiltered: false,
+    // active is determined by up and down arrow keys, as well as which item the user clicked last
     isActive: i === 0 ? true : false,
   });
 });
 
 export default function PortfolioPage() {
+  // Reducer function for isOpen state
   const isOpenReducer = (_, action) => {
     switch (action.type) {
       case "close":
@@ -39,20 +43,27 @@ export default function PortfolioPage() {
     }
   };
 
+  // the value for the search input
   const [inputValue, setInputValue] = useState("");
+  // NOTE: although it may seem like the following `selections` and `dropdowItems` contain exactly similar information regarding isSelected, they must stay separate because dropdownItems will always have a specific order and selections does not have to adhere to that order
+  // the set of values that appear next to the search box
   const [selections, setSelections] = useState(new Set());
+  // the map of items that appear in the dropdown as well as their correspnding three conditions
   const [dropdownItems, setDropdownItems] = useState(tagsMap);
+  // the toggle for the dropdown
   const [isOpen, dispatchIsOpenReducer] = useReducer(isOpenReducer, false);
 
   const inputRef = useRef(null);
   const dropdownRef = useRef(null);
   const dropdownItemRef = useRef(null);
 
+  // Focus the input, open the dropdown
   const onSearchBarClick = () => {
     inputRef.current.focus();
     dispatchIsOpenReducer({ type: "open" });
   };
 
+  // When an item in the dropdown is clicked, add it to the selected Set, mark it as selected, and mark the next active element in the dropdown if any
   const onDropdownItemClick = (e) => {
     let selection = e.target.dataset.value,
       newSelections = new Set(selections),
@@ -63,14 +74,18 @@ export default function PortfolioPage() {
       foundLastActive,
       nextActiveKey;
 
+    // Add that item to the Set of grey items displayed inside the search bar
     newSelections.add(selection);
     setSelections(newSelections);
 
+    // set item as isSelected, thus hiding the item with CSS in the dropdown without losing its place in order
     newDropdownItems.set(selection, {
       ...newDropdownItems.get(selection),
       isSelected: true,
     });
 
+    // loop through the dropdown items to find the next item in the list that should be active
+    // if there are still visible items in the Map after the selection, that item will be active, otherwise if there are no more visible items after the selection than the item right before the selection will be active, otherwise there are no more visible items in the dropdown
     for (let [tag, conditions] of newDropdownItems) {
       let { isSelected, isFiltered, isActive } = conditions;
       if (foundLastActive && nextActive) {
@@ -101,7 +116,8 @@ export default function PortfolioPage() {
     setDropdownItems(newDropdownItems);
   };
 
-  const onSearchBarSelectedTagClick = (e) => {
+  // When a selected item is clicked, remove it from the selections Set, mark is as unselected in the dropdown, and check if there are currently no visible items in the dropdown and set the recently clicked item as Active in the dropdown as well, otherwise do not change the active selection. Also check if the item would be filtered out according to the input value
+  const onSelectionClicked = (e) => {
     let selection = e.target.dataset.value,
       newSelections = new Set(selections),
       newDropdownItems = new Map(dropdownItems),
@@ -129,7 +145,8 @@ export default function PortfolioPage() {
     setDropdownItems(newDropdownItems);
   };
 
-  const handleSearchBarSearchInputChange = (e) => {
+  // When the user types in the input, ensure the dropdown is open, then check the Map of visible items and set their filter condition accordingly
+  const onInputValueChange = (e) => {
     if (!isOpen) {
       dispatchIsOpenReducer({ type: "open" });
     }
@@ -176,6 +193,7 @@ export default function PortfolioPage() {
     setDropdownItems(newDropdownItems);
   };
 
+  // When the user pressed the enter key, escape key, up arrow, or down arrow, reflect appropriate actions
   const handleOnKeyDown = (e) => {
     let keysRequiringOneVisibleItemInDropdown = [
         ENTER_KEY,
@@ -212,6 +230,7 @@ export default function PortfolioPage() {
       foundLastActive;
     switch (e.which) {
       case ENTER_KEY:
+        // on enter key, manipulate the active item - add it to the selections Set, mark it as inactive and selected in the Map, then find and mark the next active item (if there will be one)
         let selection = dropdownItemRef.current.dataset.value,
           newSelections = new Set(selections);
 
@@ -250,6 +269,7 @@ export default function PortfolioPage() {
         dispatchIsOpenReducer({ type: "close" });
         break;
       case ARROW_UP_KEY:
+        // find and activate the item before the last item that was active. if the last active item was the first item in the Map, wrap to the bottom and activate the last item
         for (let [tag, conditions] of newDropdownItems) {
           let { isSelected, isFiltered, isActive } = conditions;
           if (isSelected || isFiltered) {
@@ -283,6 +303,7 @@ export default function PortfolioPage() {
         setDropdownItems(newDropdownItems);
         break;
       case ARROW_DOWN_KEY:
+        // find and activate the item after the last item that was active. if the last active item was the last item in the Map, wrap to the top and activate the first item
         for (let [tag, conditions] of newDropdownItems) {
           let { isSelected, isFiltered, isActive } = conditions;
           if (isSelected || isFiltered) {
@@ -340,7 +361,7 @@ export default function PortfolioPage() {
           <span
             key={i}
             className={portfolioPageSerachBarSelectedTag}
-            onClick={onSearchBarSelectedTagClick}
+            onClick={onSelectionClicked}
             data-value={tag}
           >
             {tag}
@@ -352,7 +373,7 @@ export default function PortfolioPage() {
           ref={inputRef}
           value={inputValue}
           className={portfolioPageSearchBarSearchInput}
-          onChange={handleSearchBarSearchInputChange}
+          onChange={onInputValueChange}
           onKeyDown={handleOnKeyDown}
         />
         {isOpen && (
