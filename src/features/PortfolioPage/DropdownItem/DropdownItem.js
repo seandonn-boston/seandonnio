590c5f7ab4687a89b51d5e58a3a1ebce83288bae
Sean Donnellan <sean@seandonn.io>
2021-01-19 13:06:24 -0500

    Forgot memo

--- src/features/PortfolioPage/DropdownItem/DropdownItem.js
+++ src/features/PortfolioPage/DropdownItem/DropdownItem.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { memo, forwardRef } from "react";
 import cx from "classnames";
 
 import {
@@ -7,18 +7,20 @@ import {
   dropdownItemActive,
 } from "./DropdownItem.scss";
 
-export const DropdownItem = forwardRef(
-  ({ isSelected, isFiltered, isActive, tag, onClickHandler }, ref) => (
-    <div
-      ref={ref}
-      className={cx(dropdownItem, {
-        [dropdownItemHidden]: isSelected || isFiltered,
-        [dropdownItemActive]: isActive,
-      })}
-      data-value={tag}
-      onClick={(e) => onClickHandler(e)}
-    >
-      {tag}
-    </div>
+export const DropdownItem = memo(
+  forwardRef(
+    ({ isSelected, isFiltered, isActive, tag, onClickHandler }, ref) => (
+      <div
+        ref={ref}
+        className={cx(dropdownItem, {
+          [dropdownItemHidden]: isSelected || isFiltered,
+          [dropdownItemActive]: isActive,
+        })}
+        data-value={tag}
+        onClick={(e) => onClickHandler(e)}
+      >
+        {tag}
+      </div>
+    )
   )
 );
