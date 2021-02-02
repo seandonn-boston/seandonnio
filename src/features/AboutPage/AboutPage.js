4060ac4e5ab5c2c29e7f9127e7f83235045850d6
Sean Donnellan <sean@seandonn.io>
2021-02-02 17:29:37 -0500

    Built out the About me page, very barebones but I wanted to include something, some little story about the journey so far.

--- src/features/AboutPage/AboutPage.js
+++ src/features/AboutPage/AboutPage.js
@@ -1,11 +1,152 @@
 import React from "react";
 
+import { Link } from "../../global/ui/Link/Link";
+
 export default function AboutPage() {
   return (
     <>
       <h1>About</h1>
       <h2>Sean Donnellan</h2>
-      <p>Software Engineer</p>
+      <p>
+        I have a passion for Front End Engineering; if you have yet to inspect
+        this websites you would find that React + SASS are my bread and butter.
+        While my passion for the front end has not waivered much over the years,
+        the reasons why I'm passionate and the technologies powering them most
+        certainly have.
+        <br />
+        <br />I was not always a software engineer, I was a graphic designer
+        first. I earned my B.A. in Graphic Design Communications at Roger
+        Williams University in 2015, however during my tail end of my collegiate
+        career I became more interested in UI Design and then web development.
+        What exactly happens to my website designs after a coder has it?
+        <br />
+        <br />
+        <Link href="http://seandonnellan.com/images/design/flatmates_proposal.pdf">
+          FlatMates, app proposal and UI Design, 2014
+        </Link>
+        <br />
+        <br />
+        I took some intro to HTML and CSS courses and instantly loved it. I felt
+        as though I could take all of my graphic design skills and apply them to
+        an entirely new canvas. I was pretty bad at first, but I saw the
+        potential so stuck with it. I took a followup class on Responsive Design
+        and Bootstrap, and I really started to see a bigger picture taking
+        shape. With HTML, CSS, and Bootstrap I felt like I had a solid
+        foundation of web development, so I applied around and landed my first
+        position as an Associate Software Engineer at Gartner. While there I
+        helped rebuild Gartner for Marketing Leaders utilizing Hippo CMS, sent
+        out email blasts, and maintained the Digital Marketing Transit Map, all
+        in an agile scrum environment. I also began teaching myself JavaScript
+        and SASS, though they proved to be more difficult to grasp conceptually
+        than HTML and CSS.
+        <br />
+        <br />
+        <Link href="https://web.archive.org/web/20170221085142/https://www.gartner.com/marketing/">
+          Gartner for Marketing Leaders, 2017
+        </Link>
+        <br />
+        <Link href="https://www.gartner.com/en/marketing/research/data-tools/transit-map">
+          Gartner Digital Marketing Transit Map, 2017
+        </Link>
+        <br />
+        <br />I decided to leave Gartner in 2017 to be closer with my family as
+        I work through some personal health issues. When I was back on my feet I
+        decided two things - I want to live and work as a Software Engineer in
+        Boston, and I don't know enough to do that. My strong grasp on HTML,
+        CSS, and Bootstrap was enough to get a portfolio website off the ground
+        and showcase to possible employers, but none were biting. My
+        fundamentals weren't going to cut it in Boston, not to mention I am
+        still struggling with JavaScript basics and I don't even know what the
+        back end is.
+        <br />
+        <br />
+        <Link href="http://seandonnellan.com/index.html">
+          seandonnellan.com, portfolio v1, 2017
+        </Link>
+        <br />
+        <br />
+        To break the mold and enter the Boston Engineering job market with a
+        little more valuable skills, I attended General Assembly's coding
+        bootcamp Web Development Immersive, WDI (now Software Engineering
+        Immersive, SEI). Best decision of my life. Not only did I formally learn
+        JavaScript from the ground up, I was taught way more than I ever
+        anticiapted - MVC patterns with multiple tech stacks, relational
+        database management, GitHub, Node, the command line, user
+        authentication, front end javascript frameworks and libraries, UI
+        libraries, the list goes on. A massive knowledge gap had been filled. My
+        education did not stop there either. Shortly after earning my
+        certificate of completion for General Assembly, I taught myself how to
+        connect to Soptify's OAuth API, and I started learning how to build in
+        React. I quickly built out a small portfolio website as well and was
+        soon hired at Wayfair as a Software Engineer coming in through the Labs
+        program.
+        <br />
+        <br />
+        <Link href="www.seandonn-boston.github.io">
+          seandonn-boston.github.com, portfolio v2, 2018
+        </Link>
+        <br />
+        <br />
+        Wayfair Labs, for those of you who aren't aware, is a 3 month period to
+        determine your strengths as an engineer before placing you onto a team.
+        Myself and 15 other engineers were hired at the same time and worked
+        together to individually complete month long projects which were
+        reviewed and used as a basis for placement considerations - one was
+        backend focused, one was front end focused, and the final was fullstack.
+        I was initially placed on the Asia Consolidations back end team which
+        struck me as odd. I expressed my desire to work on the front end and
+        worked with my manager to find a team which I would excel in, and we
+        landed on the Storefront Discover's Navigation Team. Storefront is in
+        charge of all customer facing websites, including subsidiaries. Discover
+        was in charge of helping customers quickly find the products or services
+        they are looking for, and the Navigation Team was specifically in charge
+        of the Header and Footer. I joined the team a few months before we were
+        going to decouple the Navigations code assets from a monorepo into a
+        microservice webpack environment, which we also coincided with adoption
+        React 16.8 in the process. As a result I had a hand in rebuilding the
+        components of the Header, working very closely with our Team Lead,
+        Project Manager, UI designers, QA Testers, Business Analysts, Back End
+        Ebngineers, and fellow Front End Engineers to ensure the header was
+        optimized for performance and working perfectly for the millions of
+        customers who will see it.
+        <br />
+        <br />
+        <Link href="https://www.wayfair.com/header">Wayfair Header, 2020</Link>
+        <br />
+        <br />I saw an opportunity to improve our efforts as we were developing
+        by including Accessibility into our components, something I have a
+        pet-passion for and something we were lacking in our previous build. I
+        suggested it to my manager who was surprised and relieved I knew how to
+        do it and wanted to take up the efforts, so he happily handed me the
+        tickets he had opened on the subject and I started injecting a11y
+        throughout our completed components. I also coached several other team
+        members with Accessibility best practices and eventually we adopted it
+        as a standard requirement criteria on all tickets involving new
+        components. I took this momentum and worked with it to launch an Awesome
+        Learning course (Wayfairs fancy term for training module) on
+        Accessibility. I started to manage a few other engineers on the side of
+        my normal engineering work to devise a plan to instill empathy into
+        engineers (ie, can you navigate this site with your eyes closed?) and
+        then teach them 3-4 beginner-level best practices to code and easily
+        port over into their personal work (ie, using tabindex). Things were
+        going smoothly.
+        <br />
+        <br />
+        <Link href="www.seandonn.io">seandonn.io, portfolio v3</Link>
+        <br />
+        <br />
+        Until the Wayfair layoffs of Feb 2020. I, along with hundreds of others
+        were let go just a few weeks before Covid hit. At first I really
+        struggled, then I started to build myself up again brick by brick. I
+        have been slowly building this current website as a way to demonstrate
+        some of the coding skills I have honed since General Assembly, but it
+        has been a slow process. However, I continue to build every single day
+        as well as continue to teach myself new technologies, such as Redux, to
+        improve my marketability and be prepared to add value to your company on
+        day one. I am actively looking for work in the Boston area and would
+        love to talk about any front end engineering positons you may have
+        available at this time.
+      </p>
     </>
   );
 }
